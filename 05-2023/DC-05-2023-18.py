1dd70a17d2a3b05038fd01c7d7d3a05955510a9b
Lawrence Gilligan <93933400+LawrenceGilligan@users.noreply.github.com>
2023-05-18 12:47:56 -0400

    added algo descript

--- 05-2023/DC-05-2023-18.py
+++ 05-2023/DC-05-2023-18.py
@@ -3,7 +3,7 @@ Date: 5/18/2023
 Time: 45m
 Question: Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 Example: given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
-
+Algorithm Description: The algorithm exploits the fact that for any pair of numbers a and b, if a + b = k, then k - a = b. By storing the complements encountered so far in a set, we can efficiently check if the complement of the current number has already been encountered. 
 """
 
 def sum(numbers, k):
