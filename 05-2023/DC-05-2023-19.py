8bb31c0cbedbcb53496c4efcfaa178d51ec1cc00
Lawrence Gilligan <93933400+LawrenceGilligan@users.noreply.github.com>
2023-05-23 11:23:42 -0400

    update

--- 05-2023/DC-05-2023-19.py
+++ 05-2023/DC-05-2023-19.py
@@ -2,7 +2,7 @@
 Date: 05/19/2023
 Time: 43m
 Question: Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
-Example: For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+Example: If our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 Algorithm Description: The algorithm computes the products of the elements to the left and right of each index separately, avoiding the use of division. By traversing nums twice, it effectively computes the product of all numbers except the one at each index without using division.
 
 """
@@ -11,13 +11,13 @@ def product_except_self(nums):
     n = len(nums)
     result = [1] * n
 
-    # Compute product of elements left of each index
+    # Compute elements to the left of the index
     left_product = 1
     for i in range(n):
         result[i] *= left_product
         left_product *= nums[i]
 
-    # Compute product of elements right of each index
+    # Compute elements to the right of the index
     right_product = 1
     for i in range(n - 1, -1, -1):
         result[i] *= right_product
